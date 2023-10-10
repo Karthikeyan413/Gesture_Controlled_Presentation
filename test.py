c643b851a89498004f036719397149b2296e96e5
Shankaranaarayanan <shankaranaarayananthiru0906@gmail.com>
2023-10-10 09:29:46 +0530

    read slides from Presentation folder

--- test.py
+++ test.py
@@ -1,8 +1,18 @@
 from pptx import Presentation
 import win32com.client
+import os
+
+
+curPath = os.path.dirname(os.path.abspath(__file__))
+folderPath = os.path.join(curPath,"Presentation")
+
+
 Application = win32com.client.Dispatch("PowerPoint.Application")
-Presentation = Application.Presentations.Open(r"C:\Users\Oomkara\Documents\DAA NQueens project.pptx")
-Presentation.Slides[0].Export(r"D:\projects\PPT_opencv\Presentation\1.jpg", "JPG")
+Presentation = Application.Presentations.Open(os.path.join(folderPath,"MLT Project.pptx"))
+
+for i in range(len(Presentation.slides)):
+    Presentation.Slides[i].Export(os.path.join(folderPath,f"{i}.jpg"), "JPG")
+
 Application.Quit()
 Presentation =  None
 Application = None
